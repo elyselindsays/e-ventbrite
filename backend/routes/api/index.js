5b50865b0cd32f555b220db010531f2935fe8d5a
Elyse Steingold <elyselindsays@gmail.com>
2020-12-17 12:48:44 -0500

    login routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+
+
 
 
 
