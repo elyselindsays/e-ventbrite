6b3fe828236733fd3fdf2cda285b89e891a56188
Elyse Steingold <elyselindsays@gmail.com>
2020-12-17 12:29:00 -0500

    user auth test routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,42 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-})
+
+
+
+
+
+
+
+/**************** USER AUTH TESTING ROUTES *****************/
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   })
+
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+
+// router.get('/restore-user', restoreUser, (req, res) => {
+//   return res.json(req.user)
+// });
+
+
+// router.get('/require-auth', requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body });
+// })
 
 
 
