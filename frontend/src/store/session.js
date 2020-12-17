2be414ce451ee59c339f67d44923f2bd6c8c5563
Elyse Steingold <elyselindsays@gmail.com>
2020-12-17 17:37:58 -0500

    thunk part 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,72 @@
+// This file will contain all the actions specific to the session user's information and the session user's Redux reducer.
+
+
+import { fetch } from "./csrf";
+
+const SET_SESSION_USER = '/session/SET_SESSION_USER';
+const REMOVE_SESSION_USER = 'session/REMOVE_SESSION_USER';
+
+// Create two POJO action creators.
+// One that will set the session user in the session slice of state to the action creator's input parameter, 
+const setSessionUser = (user) => ({
+  type: SET_SESSION_USER,
+  payload: user
+})
+
+// and another that will remove the session user. Their types should be extracted as a constant and used by the action creator and the session reducer.
+const removeSessionUser = () => ({
+  type: REMOVE_SESSION_USER,
+
+});
+
+
+// You need to call the API to login then set the session user from the response, so add a thunk action for the POST / api / session.
+
+
+export const getSessionUser = (user) => async (dispatch) => {
+  // Make sure to use the custom fetch function from frontend / src / store / csrf.js.
+  // The POST / api / session route expects the request body to have a key of credential with an existing username or email and a key of password.
+  const { credential, password } = user;
+  const res = await fetch(`/api/session`, {
+    method: 'POST',
+    body: JSON.stringify({
+      credential, password
+    })
+
+  });
+
+  // After the response from the AJAX call comes back, dispatch the action for setting the session user to the response's data.
+
+  dispatch(setSessionUser(res.data.user));
+  return res;
+
+}
+
+
+
+
+// Export the login thunk action, and export the reducer as the default export.
+
+
+
+
+const initialState = { user: null };
+
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_SESSION_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_SESSION_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state
+  }
+}
+
+export default sessionReducer;
